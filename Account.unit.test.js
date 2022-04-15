6c19fdf76782bfafca08fc15287b0cb28ffd9c0d
ren132710 <ren132710@gmail.com>
2022-04-15 16:52:06 -0500

    unit tests pass

--- Account.unit.test.js
+++ Account.unit.test.js
@@ -1,69 +1,84 @@
 const Account = require('./Account')
 const FileSystem = require('./FileSystem')
 
-describe('Account Integration Tests', () => {
+//create a dummy account
+async function createAccount(name, balance) {
+  const spy = jest.spyOn(FileSystem, 'read').mockReturnValueOnce(Promise.resolve(balance))
+  const account = await Account.find(name)
+  spy.mockRestore()
+  return account
+}
+
+describe('Account Unit Tests', () => {
   beforeEach(() => {
     jest.restoreAllMocks()
   })
 
   test('should successfully create a new account with balance 0', async () => {
+    //given
     const name = 'Clark_Create'
-    const spy = jest.spyOn(FileSystem, 'write').mockReturnValue(Promise.resolve())
+    const spy = jest.spyOn(FileSystem, 'write')
+    //when
     const account = await Account.create(name)
-
+    //then
     expect(account.name).toBe(name)
     expect(account.balance).toBe(0)
-    expect(account.filePath).toBe(`./accounts/${name}`)
-    // expect(spy).toBeCalledWith(account.filePath, 0)
+    expect(account.filePath).toBe(`accounts/${name}.txt`)
+    expect(spy).toBeCalledWith(account.filePath, 0)
+    // spy.mockRestore()
   })
 
-  test *
-    ('should successfully deposit into an existing account',
-    async () => {
-      const name = 'Davis_Deposit'
-      const account = await Account.create(name)
+  test('should successfully deposit into an existing account', async () => {
+    //given
+    const name = 'Davis_Deposit'
+    const depositAmount = 100
+    const spy = jest.spyOn(FileSystem, 'write')
+    const account = await Account.create(name)
+    //when
+    await account.deposit(depositAmount)
+    //then
+    expect(account.balance).toBe(depositAmount)
+    expect(account.filePath).toBe(`accounts/${name}.txt`)
+    expect(spy).toBeCalledWith(account.filePath, depositAmount)
+  })
 
-      await account.deposit(100)
-      expect(account.balance).toBe(100)
-      expect(fs.readFileSync(account.filePath).toString()).toBe('100')
+  test('should successfully withdraw from an existing account', async () => {
+    //given
+    const name = 'White_Withdraw'
+    const startingBalance = 100
+    const withdrawAmount = 25
+    const spy = jest.spyOn(FileSystem, 'write')
+    const account = await createAccount(name, startingBalance)
+    //when
+    await account.withdraw(withdrawAmount)
+    //then
+    expect(account.balance).toBe(startingBalance - withdrawAmount)
+    expect(account.filePath).toBe(`accounts/${name}.txt`)
+    expect(spy).toBeCalledWith(account.filePath, startingBalance - withdrawAmount)
+  })
 
-      await account.deposit(50)
-      expect(account.balance).toBe(150)
-      expect(fs.readFileSync(account.filePath).toString()).toBe('150')
+  test('should prevent withdraw when insufficient funds', async () => {
+    //given
+    const name = 'White_Withdraw'
+    const startingBalance = 25
+    const withdrawAmount = 100
+    const spy = jest.spyOn(FileSystem, 'write')
+    const account = await createAccount(name, startingBalance)
+    //then
+    await expect(account.withdraw(withdrawAmount)).rejects.toThrow()
+    expect(account.balance).toBe(startingBalance)
+    expect(spy).not.toBeCalled
+  })
 
-      await account.deposit(25)
-      expect(account.balance).toBe(175)
-      expect(fs.readFileSync(account.filePath).toString()).toBe('175')
-    })
-
-  test *
-    ('should successfully withdraw from an existing account',
-    async () => {
-      const name = 'White_Withdraw'
-      const account = await Account.create(name)
-      await account.deposit(100)
-      await account.withdraw(25)
-      expect(account.balance).toBe(75)
-      expect(fs.readFileSync(account.filePath).toString()).toBe('75')
-    })
-
-  test *
-    ('should find an existing account',
-    async () => {
-      const name = 'Flores_Find'
-      const balance = 25
-      fs.writeFileSync(`accounts/${name}.txt`, balance.toString())
-      const foundAccount = await Account.find(name)
-      expect(foundAccount.name).toBe(name)
-      expect(foundAccount.balance).toBe(balance)
-      expect(fs.readFileSync(foundAccount.filePath).toString()).toBe('25')
-    })
-
-  test *
-    ('should return undefined if account not found',
-    async () => {
-      const name = 'Flores_Undefined'
-      const account = await Account.find(name)
-      expect(account).toBeUndefined()
-    })
+  test('should return undefined if account not found', async () => {
+    //given
+    const name = 'Flores_Undefined'
+    const spy = jest.spyOn(FileSystem, 'read')
+    //when
+    const account = await Account.find(name)
+    //then
+    expect(spy).toHaveBeenCalled
+    expect(account).toBeUndefined()
+    spy.mockRestore()
+  })
 })
